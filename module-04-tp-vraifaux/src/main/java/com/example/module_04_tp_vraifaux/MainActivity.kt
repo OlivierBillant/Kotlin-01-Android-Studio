b0b26c2a6a5963312a304905b4a5aa749ba86008
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-06 15:12:49 +0200

    Change : factorisation

--- module-04-tp-vraifaux/src/main/java/com/example/module_04_tp_vraifaux/MainActivity.kt
+++ module-04-tp-vraifaux/src/main/java/com/example/module_04_tp_vraifaux/MainActivity.kt
@@ -7,54 +7,58 @@ import com.example.module_04_tp_vraifaux.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var amb: ActivityMainBinding
+    var score = 0
+    var indiceQuestion = 0
+    var listeQuestionReponse = ArrayList<QuestionReponse>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        On initialise la vue
-        var score = 0
-        var indiceQuestion = 0
-        var listeQuestionReponse = ArrayList<QuestionReponse>()
         listeQuestionReponse.add(QuestionReponse("Est ce qu'une Vache ?", true))
         listeQuestionReponse.add(QuestionReponse("Faut-il moins de Max ?", true))
         listeQuestionReponse.add(QuestionReponse("Le pompe est-ce la vie?", false))
         listeQuestionReponse.add(QuestionReponse("La vie est-ce la vie?", true))
         listeQuestionReponse.add(QuestionReponse("La vie est-ce le pompe?", false))
+//        On initialise la vue
         amb = DataBindingUtil.setContentView(this, R.layout.activity_main)
         amb.q = listeQuestionReponse[indiceQuestion]
-        amb.textViewScore.text = score.toString()
+        scoring()
 
         amb.buttonVrai.setOnClickListener {
-            if (amb.q == null) {
-                return@setOnClickListener;
+            if (amb.q == null || indiceQuestion == listeQuestionReponse.size) {
+                return@setOnClickListener
             }
-            if (amb.q?.reponse == true) {
-                score++
-                amb.textViewScore.text = score.toString()
-            }
+            checkAnswer(true)
             indiceQuestion++
-            if (indiceQuestion == listeQuestionReponse.size) {
-                amb.textViewScore.text = "Vous avez fait un score de $score"
-                amb.TextViewQuestion.text = ""
-            } else {
-                amb.q = listeQuestionReponse[indiceQuestion]
-            }
+            endGame()
         }
         amb.buttonFaux.setOnClickListener {
-            if (amb.q == null) {
-                return@setOnClickListener;
+            if (amb.q == null || indiceQuestion == listeQuestionReponse.size) {
+                return@setOnClickListener
             }
-                if (amb.q?.reponse == false) {
-                    score++
-                    amb.textViewScore.text = score.toString()
-                }
-                indiceQuestion++
-            if (indiceQuestion == listeQuestionReponse.size) {
-                amb.textViewScore.text = "Vous avez fait un score de $score"
-                amb.TextViewQuestion.text = ""
-            } else {
-                amb.q = listeQuestionReponse[indiceQuestion]
-            }
+            checkAnswer(false)
+            indiceQuestion++
+            endGame()
         }
     }
 
+    fun scoring() {
+        amb.textViewScore.text = "${score.toString()}/${listeQuestionReponse.size}"
+    }
+
+    fun endGame() {
+        if (indiceQuestion == listeQuestionReponse.size) {
+            amb.textViewScore.text =
+                "Vous avez fait un score de $score/${listeQuestionReponse.size}"
+            amb.TextViewQuestion.text = ""
+        } else {
+            amb.q = listeQuestionReponse[indiceQuestion]
+        }
+    }
+
+    fun checkAnswer(userResponse: Boolean) {
+        if (amb.q?.reponse == userResponse) {
+            score++
+            scoring()
+        }
+    }
 }
 
