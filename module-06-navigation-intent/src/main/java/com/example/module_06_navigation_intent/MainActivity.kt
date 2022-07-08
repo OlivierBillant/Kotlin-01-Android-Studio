ffa404eedba3e9e52110daef53851cff06ae4262
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-08 15:25:38 +0200

    ADD : Demo Nav Intent et permission

--- module-06-navigation-intent/src/main/java/com/example/module_06_navigation_intent/MainActivity.kt
+++ module-06-navigation-intent/src/main/java/com/example/module_06_navigation_intent/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.module_06_navigation_intent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +26,36 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
-//    Intent implicite, destination connue
+//    Intent implicite, destination inconnue
+        val buttonCall = findViewById<Button>(R.id.buttonCall)
+        buttonCall.setOnClickListener {
+            val uri = Uri.parse("tel:0612345678")
+            val intentToCall = Intent(Intent.ACTION_DIAL, uri)
+            startActivity(intentToCall)
+        }
 
+        //   Permission dangereuse
+        val buttonSms = findViewById<Button>(R.id.buttonSMS)
+        buttonSms.setOnClickListener {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.SEND_SMS),
+                22
+            )
+        }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 22 && grantResults[0] == PERMISSION_GRANTED) {
+            val intentToSms = Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:06123456768"))
+            intentToSms.putExtra("sms_body", "Salut Max !")
+            startActivity(intentToSms)
+        }
+    }
+
 }
